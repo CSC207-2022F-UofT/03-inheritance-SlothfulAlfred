8a0ed03ef9ca0e6d4110e92193ac6e1cc75b33d6
Alfred Mikhael <alfred.mikhael@mail.utoronto.ca>
2022-09-29 23:41:08 -0400

    Completed exercise 3.

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -6,88 +6,81 @@
  */
 
 public abstract class Bag {
-    /*
-     * TODO: Create the following private instance variables
-     *       - a String named color
-     *       - an int named numberOfContents
-     *       - an int named capacity
-     *       - an array of Strings named contents
-     */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
+    Bag(String color, int capacity) {
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[capacity];
+    }
 
+    public String getColor() {
+        return this.color;
+    }
 
+    public int getNumberOfContents() {
+        return this.numberOfContents;
+    }
 
-    /*
-     * TODO: Create a constructor that takes two arguments:
-     *       - a String representing the Bag's colour
-     *       - an int representing the Bag's capacity
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setColor(String s) {
+        this.color = s;
+    }
+
+    /**
+     * Adds a given item to the Bad if there is room.
      *
-     * The other attributes (private instance variables) should
-     * be empty (e.g. numberOfContents is 0 and an empty String array for
-     * its contents.)
+     * @param item the item to be added
+     * @return true if the item was added and false otherwise
      */
-
-
-
-
-    /*
-     * TODO: Create a variety of 'getter' functions.
-     *       These should be named:
-     *           - getColor
-     *           - getNumberOfContents
-     *           - getCapacity
-     */
-
-
-
-
-    /*
-     * TODO: Create a setter function called setColor which sets the
-     *       color of this bag to the given color.
-     */
-
-
-
-
-
-    /*
-     * TODO: Create a method called addItem that takes in a String
-     *       representing an item in the Bag.
-     *       The item is added into the Bag if the number of items
-     *       in the bag is < the capacity of the Bag.
-     *       Remember to modify numberOfContents accordingly.
-     *
-     *       This method should return true if the item was added
-     *       and false otherwise.
-     */
+    public boolean addItem(String item) {
+        if (numberOfContents < capacity) {
+            contents[numberOfContents] = item;
+            numberOfContents++;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 
 
 
     /**
-     * TODO: Create a method called popItem that returns a String.
-     *       The string should be the last item added to this Bag
-     *       and the item should be removed from this Bag.
-     *       Remember to modify numberOfContents accordingly.
+     * Returns the last item added to this Bad and removes it.
      *
-     * If there are no items in this Bag, return null.
-     *
-     * @return
+     * @return The last item added to this Bad, or null if this Bag is empty
      */
+    public String popItem() {
+        if (contents.length == 0) {
+            return null;
+        }
+        numberOfContents--;
+        return contents[numberOfContents];
+    }
 
 
 
 
 
     /**
-     * Increase this bag's capacity by n.
+     * Increase this bag's capacity by a positive integer n.
      *
      * @param n the amount to increase this Bag's capacity by
      */
     public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+        capacity += n;
+        String[] new_contents = new String[capacity];
+        System.arraycopy(contents, 0, new_contents, 0, contents.length);
+        this.contents = new_contents;
     }
 
     /**
